4c2527eecdd93bd1e563c063719f1ecc7eadab63
Raitis <pingy030799@gmail.com>
2019-04-01 20:44:12 +0300

    edit added

--- name/app/controllers/articles_controller.rb
+++ name/app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+  		@article = Article.find(params[:id])
+	end
+
 	def create
 		@article = Article.new(article_params)
 		 
@@ -22,6 +26,16 @@ class ArticlesController < ApplicationController
   		end
 	end
 	
+	def update
+  		@article = Article.find(params[:id])
+ 
+  		if @article.update(article_params)
+    		redirect_to @article
+  		else
+    		render 'edit'
+  		end
+	end
+
 private
   	def article_params
     	params.require(:article).permit(:title, :description, :customer, :user)
