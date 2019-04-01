fa23fc800caa9c7a1871e2018b162b3914da806e
Raitis <pingy030799@gmail.com>
2019-04-01 18:30:55 +0300

    /articles and articles/new added

--- name/config/routes.rb
+++ name/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-	root 'pages#home'
+  get 'welcome/index'
 
+  	resources :articles
+
+	 root 'welcome#index'
   get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
