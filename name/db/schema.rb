2ac26894f31bb20475cd184e48cecc0231dd48e6
Raitis <pingy030799@gmail.com>
2019-04-02 18:20:33 +0300

    creating and viewing comments

--- name/db/schema.rb
+++ name/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_01_153330) do
+ActiveRecord::Schema.define(version: 2019_04_02_143620) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 2019_04_01_153330) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
 end
