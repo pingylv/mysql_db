17972220660b67358ae032e0df8590ba6a26109e
Raitis <pingy030799@gmail.com>
2019-05-08 19:09:47 +0300

    client/new page created

---
+++ name/app/controllers/clients_controller.rb
@@ -0,0 +1,10 @@
+class ClientsController < ApplicationController
+
+def new
+end
+
+def create
+	render plain: params[:client].inspect
+end
+
+end
--- name/config/routes.rb
+++ name/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   		resources :comments
 	end
 
+	resources :clients
+
 	 root 'welcome#index'
   get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
