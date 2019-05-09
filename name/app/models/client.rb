7ab62f2152c0753856c14fdf8e3555631b84c030
Raitis <pingy030799@gmail.com>
2019-05-09 18:31:57 +0300

    validation custom message for presence

--- name/app/models/client.rb
+++ name/app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
-  validates :vards, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
-  validates :uzvards, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
-  validates :tel_nummurs, presence: true, length: { in: 8..12 }
-  validates :adrese, presence: true
+  validates :vards, presence: { message: "ir obligati jaievada" }, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
+  validates :uzvards, presence: { message: "ir obligati jaievada" }, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
+  validates :tel_nummurs, presence: { message: "ir obligati jaievada" }, length: { in: 8..12 }
+  validates :adrese, presence: { message: "ir obligati jaievada" }
 end
