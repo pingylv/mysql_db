2895aecb89fce84e9d3f2453b4de283080a50cc4
Raitis <pingy030799@gmail.com>
2019-05-09 18:20:18 +0300

    client phone is 8-12 characters long

--- name/app/models/client.rb
+++ name/app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
   validates :vards, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
   validates :uzvards, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
-  validates :tel_nummurs, presence: true, length: { minimum: 8 }
+  validates :tel_nummurs, presence: true, length: { in: 8..12 }
   validates :adrese, presence: true
 end
