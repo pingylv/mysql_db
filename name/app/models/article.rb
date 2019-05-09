047ffa67eee3eb6fcbf5edab989258967c757701
Raitis <pingy030799@gmail.com>
2019-05-09 18:13:54 +0300

    extra changes in validation for both articles and clients

--- name/app/models/article.rb
+++ name/app/models/article.rb
@@ -1,8 +1,7 @@
 class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
-	validates :title, presence: true,
-        length: { minimum: 5 }
+	validates :title, presence: true, length: { minimum: 5 }
 	validates :description, presence: true 
-	validates :customer, presence: true
+	validates :customer, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "can only contain letters" }
 	validates :user, presence: true    
 end
--- name/app/models/client.rb
+++ name/app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
-  validates :vards, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "tikai burtus var izmantot" }
-  validates :uzvards, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "tikai burtus var izmantot" }
+  validates :vards, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
+  validates :uzvards, presence: true, length: { minimum: 3 }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
   validates :tel_nummurs, presence: true, length: { minimum: 8 }
   validates :adrese, presence: true
 end
