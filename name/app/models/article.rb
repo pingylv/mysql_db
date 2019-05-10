65b3c85bdb3e085c3c7f080a23300ee327b4e9f5
Raitis <pingy030799@gmail.com>
2019-05-10 17:09:24 +0300

    article cant have a customer with no spaces (as of now)

--- name/app/models/article.rb
+++ name/app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :description, presence: true 
-	validates :customer, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "can only contain letters" }
+	validates :customer, presence: true
 	validates :user, presence: true    
 end
