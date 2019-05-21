1e6db48405aef5a85e7eef98bc016ae276da7918
Raitis <pingy030799@gmail.com>
2019-05-21 17:30:19 +0300

    error message partial translation (display part)

--- name/app/models/article.rb
+++ name/app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
 	has_many :comments, dependent: :destroy
-	validates :title, presence: true, length: { minimum: 5 }
-	validates :description, presence: true 
-	validates :customer, presence: true
-	validates :user, presence: true    
+	validates :title, presence: { message: "ir obligati jaievada" }, length: { minimum: 5 }
+	validates :description, presence: { message: "ir obligati jaievada" } 
+	validates :customer, presence: { message: "ir obligati jaievada" }
+	validates :user, presence: { message: "ir obligati jaievada" }    
 end
