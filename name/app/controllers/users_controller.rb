387df879a62caa22a5d3e083bd6e964582a6f9bd
Raitis <pingy030799@gmail.com>
2019-06-06 18:17:04 +0300

    register/login goes to /articles

--- name/app/controllers/users_controller.rb
+++ name/app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 		user = User.new(user_params)
 		if user.save
 			session[:user_id] = user.id 
-			redirect_to '/dashboard'
+			redirect_to '/articles'
 		else
 			flash[:register_errors] = user.errors.full_messages
 			redirect_to '/'
