a1a7c7c05d255a4806dd42e758c8f9c29337fd2b
pingylv <pingy030799@gmail.com>
2019-06-25 10:01:33 +0300

    komentars visualy to piezime // filtration for user and customer when creating fixed

--- name/app/models/sudziba.rb
+++ name/app/models/sudziba.rb
@@ -2,7 +2,7 @@ class Sudziba < ApplicationRecord
 	has_many :piezimes, dependent: :destroy
 	validates :title, presence: { message: "ir obligati jaievada" }, length: { minimum: 5 } 
 	validates :description, presence: { message: "ir obligati jaievada" } 
-	validates :customer, presence: { message: "ir obligati jaievada" }, length:  {maximum: 30}, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
-	validates :user, presence: { message: "ir obligati jaievada" }, length:  {maximum: 30}, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" }
+	validates :customer, presence: { message: "ir obligati jaievada" }, length:  {maximum: 30}
+	validates :user, presence: { message: "ir obligati jaievada" }, length:  {maximum: 30}
 	validates :status, presence: { message: "ir obligati jaievada" }, format: { with: /\A[a-zA-Z]+\z/, message: "var saturet tikai burtus" } 
 end
